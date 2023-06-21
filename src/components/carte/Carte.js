6ade77fcfd2869eb5f3832d649bea68278841761
Software engeneer <kikonistephane@gmail.com>
2023-06-21 18:10:17 +0200

    kk

--- src/components/carte/Carte.js
+++ src/components/carte/Carte.js
@@ -11,7 +11,7 @@ const Carte = () => {
                     <div className='title-small'>
                         Au coeur des territoires
                     </div>
-                    
+
                     <div className='desc'>
                         Les besoins émergent, partout, chacun avec ses spécificités locales et territoriales.
                         Le Groupe SOS est implanté dans plus de 40 pays pour répondre aux besoins des plus vulnérables, des
@@ -19,6 +19,7 @@ const Carte = () => {
                         le terrain pour construire avec les habitants et les acteurs locaux
                         des solutions à leurs problématiques écologiques et sociales.
                     </div>
+                    
                     <div className='button'>
                         <button className='btn'>Découvrir</button>
                     </div>
